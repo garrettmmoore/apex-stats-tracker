3e3f26b94e236a3f2236d0ee485111777cf3fd47
Garrett Moore <garrettmarkmoore@gmail.com>
2019-10-21 00:21:01 -0700

    Configure API authentication headers

--- routes/profile.js
+++ routes/profile.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const fetch = require("node-fetch");
 
-router.get("/:platform/:gamertag", (req, res) => {
-  console.log(req.params.platform, req.params.gamertag);
-  console.log(req);
-  res.send("Hello");
+router.get("/:platform/:gamertag", async (req, res) => {
+  try {
+    const headers = {
+      "TRN-Api-Key": process.env.TRACKER_API_KEY
+    };
+    const response = await fetch(
+      `${process.env.TRACKER_API_URL}/profile${platform}/${gamertag}`,
+      headers
+    );
+  } catch (err) {
+    console.error(error);
+  }
 });
 
 module.exports = router;
