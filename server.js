07c8a4fe3d110bf9d84f300f19c6eaf309139d54
Garrett Moore <garrettmarkmoore@gmail.com>
2019-10-21 00:08:07 -0700

    Add middleware for dev logging

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ dotenv.config({ path: "./config.env" });
 
 const app = express();
 
+// Middleware for dev logging
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// Profile routes
+app.use("/api/v1/proile", require("./routes/profile"));
+
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
