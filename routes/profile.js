28d5c6f464b82b4a2224f0b8bfd6053ba6ffa5de
Garrett Moore <garrettmarkmoore@gmail.com>
2019-10-21 00:55:16 -0700

    Update syntax styling to use single quotes

--- routes/profile.js
+++ routes/profile.js
@@ -1,11 +1,11 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const fetch = require("node-fetch");
+const fetch = require('node-fetch');
 
-router.get("/:platform/:gamertag", async (req, res) => {
+router.get('/:platform/:gamertag', async (req, res) => {
   try {
     const headers = {
-      "TRN-Api-Key": process.env.TRACKER_API_KEY
+      'TRN-Api-Key': process.env.TRACKER_API_KEY
     };
 
     const { platform, gamertag } = req.params;
@@ -22,7 +22,7 @@ router.get("/:platform/:gamertag", async (req, res) => {
 
     if (data.errors && data.errors.length > 0) {
       return res.status(400).json({
-        message: "Profile Not Found"
+        message: 'Profile Not Found'
       });
     }
 
@@ -31,7 +31,7 @@ router.get("/:platform/:gamertag", async (req, res) => {
   } catch (err) {
     console.error(err);
     res.status(500).json({
-      message: "Server Error"
+      message: 'Server Error'
     });
   }
 });
--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-const express = require("express");
-const morgan = require("morgan");
-const dotenv = require("dotenv");
+const express = require('express');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
 
 // Load env variables
-dotenv.config({ path: "./config.env" });
+dotenv.config({ path: './config.env' });
 
 const app = express();
 
 // Middleware for dev logging
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
 
 // Profile routes
-app.use("/api/v1/profile", require("./routes/profile"));
+app.use('/api/v1/profile', require('./routes/profile'));
 
 const port = process.env.PORT || 8000;
 
