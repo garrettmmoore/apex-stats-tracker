8e7d2612f84e9c5015c24379a761e0efcf643085
Garrett Moore <garrettmarkmoore@gmail.com>
2019-10-24 01:34:55 -0700

    Update form to allow user to submit search queries

--- client/src/components/Search.vue
+++ client/src/components/Search.vue
@@ -28,6 +28,30 @@
 </template>
 
 <script>
+export default {
+  name: "Search",
+  data() {
+    return {
+      platform: "psn",
+      gamertag: ""
+    };
+  },
+  beforeCreate() {
+    document.body.className = "body-bg-image";
+  },
+  methods: {
+    onSubmit() {
+      if (!this.gamertag) {
+        this.$toasted.show("Please enter a gamertag", {
+          duration: 3000,
+          icon: "exclamation-circle"
+        });
+      } else {
+        this.$router.push(`/profile/${this.platform}/${this.gamertag}`);
+      }
+    }
+  }
+};
 </script>
 
 <style>
