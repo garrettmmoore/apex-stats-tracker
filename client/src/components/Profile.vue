6f2bf03bd03ecd767e5e36d99698d47eedbbc3fa
Garrett Moore <garrettmarkmoore@gmail.com>
2019-10-24 01:31:08 -0700

    Update Profile to fetch user data from Tracker API

--- client/src/components/Profile.vue
+++ client/src/components/Profile.vue
@@ -12,6 +12,34 @@
 </template>
 
 <script>
+import axios from "axios";
+export default {
+  name: "Profile",
+  data() {
+    return {
+      loading: false,
+      error: null,
+      profileData: null
+    };
+  },
+  beforeCreate() {
+    document.body.className = "body-bg-no-image";
+  },
+  async created() {
+    this.loading = true;
+    try {
+      const res = await axios.get(
+        `/api/v1/profile/${this.$route.params.platform}/${this.$route.params.gamertag}`
+      );
+      this.profileData = res.data.data;
+      console.log(this.profileData);
+      this.loading = false;
+    } catch (err) {
+      this.loading = false;
+      this.error = err.response.data.message;
+    }
+  }
+};
 </script>
 
 <style>
