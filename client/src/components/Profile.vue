0ae6ce59bc7d585442826b9de3597edbdc5edd07
Garrett Moore <garrettmarkmoore@gmail.com>
2019-10-24 01:28:56 -0700

    Add Profile component template

---
+++ client/src/components/Profile.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+    <div v-if="loading">
+      <h3>Loading...</h3>
+    </div>
+    <div v-if="error">
+      <h1>${{error}}</h1>
+      <router-link to="/">Go Back</router-link>
+    </div>
+    <div v-if="profileData" class="container">{{profileData.metadata.activeLegendName}}</div>
+  </section>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
