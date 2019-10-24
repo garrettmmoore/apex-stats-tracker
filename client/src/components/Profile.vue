9a69f93fa44ec83fca5d09efb96ab10d3733d4f2
Garrett Moore <garrettmarkmoore@gmail.com>
2019-10-24 01:59:15 -0700

    Add style to format profile data

--- client/src/components/Profile.vue
+++ client/src/components/Profile.vue
@@ -62,5 +62,77 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.container {
+  background: rgba(0, 0, 0, 0.5);
+  color: #fff;
+  max-width: 700px;
+  margin: 1rem auto;
+  padding: 2rem 1.5rem;
+  border-radius: 20px;
+}
+h1.gamertag {
+  font-size: 2rem;
+  background: rgba(0, 0, 0, 0.6);
+  padding: 0.3rem 0.5rem;
+  text-align: center;
+  border-radius: 20px;
+  margin-bottom: 3rem;
+  display: flex;
+  align-items: center;
+}
+a {
+  display: inline-block;
+  margin-top: 2rem;
+  border: #fff 2px solid;
+  padding: 0.5rem 0.8rem;
+}
+a:hover {
+  border: #ccc 2px solid;
+  color: #ccc;
+}
+.platform-avatar {
+  width: 40px;
+  margin-right: 0.7rem;
+}
+img {
+  width: 100%;
+}
+.grid {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 1rem;
+}
+li {
+  background: rgba(0, 0, 0, 0.6);
+  padding: 1rem;
+  margin-bottom: 0.7rem;
+  border-radius: 10px;
+}
+li p {
+  font-size: 2rem;
+}
+li:first-child p {
+  font-size: 1.5rem;
+}
+li span {
+  font-size: 1rem;
+  color: #ccc;
+}
+@media (max-width: 500px) {
+  h1 {
+    font-size: 1.5rem;
+    display: block;
+    text-align: center;
+  }
+  .platform-avatar {
+    display: none;
+  }
+  .grid {
+    grid-template-columns: 1fr;
+  }
+  li p {
+    font-size: 1.5rem;
+  }
+}
 </style>
